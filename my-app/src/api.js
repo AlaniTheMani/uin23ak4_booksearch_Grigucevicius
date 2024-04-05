0817e5ba0efad3640e0bd5d6a8630916d4c20e6e
AlaniTheMani <alan.grig@icloud.com>
2024-04-05 07:01:05 +0200

    laget en api fil

---
+++ my-app/src/api.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://openlibrary.org/search.json';
+
+async function searchBooks(query) {
+  try {
+    const response = await fetch(`${API_URL}?title=${query}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch books');
+    }
+    const data = await response.json();
+    return data.docs;
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    return [];
+  }
+}
+
+export { searchBooks };
