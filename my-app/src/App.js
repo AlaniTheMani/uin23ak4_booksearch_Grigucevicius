b173e9770074230bec4003cea3a21fde6dd94cac
AlaniTheMani <alan.grig@icloud.com>
2024-04-05 07:11:27 +0200

    oppdaterte handlesearch funksjonen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,22 +7,14 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   
   const handleSearch = async (query) => {
-    if (query.length >= 3) {
-      const books = await searchBooks(query);
-      setSearchResults(books);
-    } else {
-      setSearchResults([]);
-    }
+    const books = await searchBooks(query);
+    setSearchResults(books);
   };
 
   return (
     <div>
       <SearchBar handleSearch={handleSearch} />
-      {searchResults.length > 0 ? (
-        <BookList books={searchResults} />
-      ) : (
-        <h2>No search results</h2>
-      )}
+      <BookList books={searchResults} />
     </div>
   );
 }
